685db9901b100ff9f397d0798c0cff3c15406f6c
Joan Albert Segura <jalbertsr@protonmail.ch>
2017-10-30 20:32:17 +0100

    webpack configuration finished

---
+++ src/Components/App/App.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import './App.scss'
+
+const App = () => <h1>Hola!</h1>
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const cssModules = 'modules&importLoaders=1&localIdentName=[name]__[local]__[hash:base64:5]'
+module.exports = {
+  resolve: {
+    extensions: ['.js', '.jsx', '.css']
+  },
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      { test: /(\.js|jsx)$/, exclude: /node_modules/, loaders: ['babel-loader'] },
+      { test: /\.css$/, loader: `style-loader!css-loader?${cssModules}` },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader?modules&importLoader=2&sourceMap&localIdentName=[name]__[local]___[hash:base64:5]!sass-loader'
+        })
+      }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true,
+    port: 3000
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './src/assets/index.html' }),
+    new ExtractTextPlugin('style.css', { allChunks: true })
+  ]
+}
