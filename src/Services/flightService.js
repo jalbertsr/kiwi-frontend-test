5511b7125ff43065a56b5ef79e96848cf3cbb60e
Joan Albert Segura <jalbertsr@protonmail.ch>
2017-11-12 17:49:33 +0100

    included urlbase inside service object

--- src/Services/flightService.js
+++ src/Services/flightService.js
@@ -1,15 +1,15 @@
 import axios from 'axios'
-const baseUrl = 'https://api.skypicker.com'
 
 export default {
+  baseUrl: 'https://api.skypicker.com',
   getFlights: function (_from, to, dateFrom, dateTo) {
-    const url = `${baseUrl}/flights?v=2&locale=en&flyFrom=${_from}&to=${to}&dateFrom=${dateFrom}&dateTo=${dateTo}`
+    const url = `${this.baseUrl}/flights?v=2&locale=en&flyFrom=${_from}&to=${to}&dateFrom=${dateFrom}&dateTo=${dateTo}`
     return axios.get(url)
       .then(response => response.data)
       .catch(err => new Error(err))
   },
   getPlaces: function (query) {
-    const url = `${baseUrl}/places?term=${query}&v=2&locale=en`
+    const url = `${this.baseUrl}/places?term=${query}&v=2&locale=en`
     return axios.get(url)
       .then(response => response.data)
       .catch(err => new Error(err))
